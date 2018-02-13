35fb64ffe8005450e40d22bf3dbb07e58ee1d6b8
Ediz Turcan <ediz.turcan@gmail.com>
2018-02-13 18:52:55 +0100

    euro price added

--- Profit Trailer Monitor Plus.user.js
+++ Profit Trailer Monitor Plus.user.js
@@ -4,56 +4,120 @@
 // @version      01.0.00
 // @description  Vires in Numeris
 // @author       Ediz Turcan
-// @match        https://turcan.de:8081/*
+// @match        https://localhost:8081/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    const locale = 'en-EN';
-    const currency = 'Bits';
-    const divider = 1000000;
+    // Your local language code, e.g. de-DE, en-EN, en-GB
+    const locale = 'de-DE';
+
+    // BTC, mBTC or Bits
+    var currency = 'Bits';
+
+    // USD or EUR
+    const fiat = 'EUR';
+
+    // Don't change anything below this line
+
+    var multiplier = 1;
+    switch (currency) {
+        case 'Bits':
+            multiplier = 1000000;
+            break;
+        case 'mBTC':
+            multiplier = 1000;
+            break;
+        default:
+            currency = 'BTC';
+            multiplier = 1;
+            break;
+    }
+
+    var euroPrice = -1;
 
     $(document).ready(function() {
         console.log("Profit Trailer Monitor Plus v01.0.00 loaded.");
-
-        convert();
+        euroPriceJob();
+        convertJob();
     });
 
-    function convert() {
+    function convertJob() {
         var refreshTime = 100;
 
         // Balance
-        convertId('#mBalanceVal');
+        convertFiat('#mBalanceVal', '#mBalUSDValue');
+        convertCurrency('#mBalanceVal');
 
         // Total Current Value
-        convertId('#mTotalCurrentVal');
+        convertFiat('#mTotalCurrentVal', '#mTCVUSDValue');
+        convertCurrency('#mTotalCurrentVal');
 
         // Total Pending Value
-        convertId('#mTotalPendingVal');
+        convertFiat('#mTotalPendingVal', '#mTPVUSDValue');
+        convertCurrency('#mTotalPendingVal');
 
         // Profit Last Week
-        convertId('#mLastWeekProfit');
+        convertFiat('#mLastWeekProfit', '#mLastWeekProfitUSDValue');
+        convertCurrency('#mLastWeekProfit');
         showPercentageChange('#mTotalCurrentVal', '#mLastWeekProfit');
 
         // Profit Yesterday
-        convertId('#mYesterdayProfit');
+        convertFiat('#mYesterdayProfit', '#mYesterdayProfitUSDValue');
+        convertCurrency('#mYesterdayProfit');
         showPercentageChange('#mTotalCurrentVal', '#mYesterdayProfit');
 
         // Profit Today
-        convertId('#mTodayProfit');
+        convertFiat('#mTodayProfit', '#mTodayProfitUSDValue');
+        convertCurrency('#mTodayProfit');
         showPercentageChange('#mTotalCurrentVal', '#mTodayProfit');
 
-        setTimeout(convert, refreshTime);
+        setTimeout(convertJob, refreshTime);
     }
 
-    function convertId(id) {
+    function euroPriceJob() {
+        var refreshTime = 10000;
+        $.getJSON('https://api.coinmarketcap.com/v1/ticker/bitcoin/?convert=EUR', function(data) {
+            var price = Number(data[0].price_eur);
+            if(!isNaN(price)) {
+                euroPrice = price;
+            }
+            setTimeout(euroPriceJob, refreshTime);
+        });
+    }
+
+    function convertFiat(id, fiatID) {
+        if(fiat == 'USD') { return; }
+        if(fiat == 'EUR' && euroPrice == -1) { return; }
+        var price = 0;
+        switch (fiat) {
+            case 'EUR':
+                price = euroPrice;
+                break;
+            default:
+                break;
+        }
+        var value = Number($(id).attr('title'));
+        if(isNaN(value)) { return; }
+        var newPrice = value * price;
+        $(fiatID).text(newPrice.toLocaleString(locale, { maximumFractionDigits: 2 }));
+        var children = $(($(fiatID).parent().children())[0]);
+        var labels = children.children();
+        labels.each(function() {
+            var currentText = $(this).text();
+            $(this).text(currentText.replace('USD', fiat));
+        });
+    }
+
+    function convertCurrency(id) {
+        if(currency == 'BTC') { return; }
         const obj = $(id);
-        var currentValue = Number(obj.text());
+        var currentValue = Number(obj.attr("title"));
         if(!isNaN(currentValue)) {
-            currentValue *= divider;
-            obj.text(currentValue.toLocaleString(locale));
+            var newValue = currentValue * multiplier;
+            obj.text(newValue.toLocaleString(locale));
             obj.next().text(currency);
         }
     }
